01e22820a8a82d8d051e9889760570d0412e9539
André Luiz <andreluiz.97@outlook.com>
2018-02-01 08:34:29 -0200

    Adicionado favicons

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,10 +8,18 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'André Luiz CV' }
+      { hid: 'description', name: 'description', content: 'André Luiz CV' },
+      { name: 'msapplication-config', content: '/favicons/browserconfig.xml' },
+      { name: 'theme-color', content: '#2196f3' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      // Favicons
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicons/apple-touch-icon.png' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicons/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicons/favicon-16x16.png' },
+      { rel: 'manifest', href: '/favicons/manifest.json' },
+      { rel: 'mask-icon', href: '/favicons/safari-pinned-tab.svg', color: '#5bbad5' },
+      { rel: 'shortcut icon', href: '/favicons/favicon.ico' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' },
       { rel: 'stylesheet', href: 'http://cdn.materialdesignicons.com/2.1.19/css/materialdesignicons.min.css' }
     ]
