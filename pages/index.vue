25cfce3e57a37dab40d1989496d4dc49425bb43f
André Luiz <andreluiz.97@outlook.com>
2018-02-08 18:10:32 -0200

    Fechando tags dentro delas mesmas

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <v-layout justify-center>
     <v-flex xs10>
       <section>
-        <section-title title="Sobre" anchor="about"></section-title>
+        <section-title title="Sobre" anchor="about"/>
         <div class="text-xs-justify text-sm-center">
           <p>Olá, mundo! Meu nome é <b>André Luiz</b>, tenho 21 anos e sou residente de Brusque - SC.</p>
           <p>
@@ -12,49 +12,49 @@
         </div>
       </section>
       <section>
-        <section-title title="Tecnologias" anchor="technologies"></section-title>
+        <section-title title="Tecnologias" anchor="technologies"/>
         <div class="text-js-justify text-sm-center mb-5">
           <p>Algumas das tecnologias que já trabalhei ou que trabalho atualmente. Estão incluídas linguagens, sotwares, frameworks, bancos de dados, plataformas, entre outros.</p>
         </div>
         <v-layout row wrap>
-          <technology img="html5.png" name="HTML 5"></technology>
-          <technology img="css3.png" name="CSS 3"></technology>
-          <technology img="js.png" name="JavaScript"></technology>
-          <technology img="php.png" name="PHP"></technology>
-          <technology img="nodejs.png" name="Node.js"></technology>
-          <technology img="npm.png" name="npm"></technology>
-          <technology img="express.png" name="express"></technology>
-          <technology img="sequelize.png" name="Sequelize"></technology>
-          <technology img="jquery.png" name="jQuery"></technology>
-          <technology img="vuejs.png" name="Vue.js"></technology>
-          <technology img="nuxtjs.png" name="Nuxt.js"></technology>
-          <technology img="laravel.png" name="Laravel"></technology>
-          <technology img="blade.png" name="Laravel Blade"></technology>
-          <technology img="twig.png" name="Twig"></technology>
-          <technology img="sass.png" name="Sass"></technology>
-          <technology img="stylus.png" name="Stylus"></technology>
-          <technology img="vuetify.png" name="Vuetify.js"></technology>
-          <technology img="materialize.png" name="Materialize"></technology>
-          <technology img="semantic-ui.png" name="Semantic UI"></technology>
-          <technology img="photoshop.png" name="Photoshop"></technology>
-          <technology img="illustrator.png" name="Illustrator"></technology>
-          <technology img="git.png" name="Git"></technology>
-          <technology img="cpp.png" name="C++"></technology>
-          <technology img="java.png" name="Java"></technology>
-          <technology img="mysql.png" name="MySQL"></technology>
-          <technology img="postgresql.png" name="PostgreSQL"></technology>
-          <technology img="drupal.png" name="Drupal"></technology>
-          <technology img="nuvemshop.png" name="Nuvem Shop"></technology>
+          <technology img="html5.png" name="HTML 5"/>
+          <technology img="css3.png" name="CSS 3"/>
+          <technology img="js.png" name="JavaScript"/>
+          <technology img="php.png" name="PHP"/>
+          <technology img="nodejs.png" name="Node.js"/>
+          <technology img="npm.png" name="npm"/>
+          <technology img="express.png" name="express"/>
+          <technology img="sequelize.png" name="Sequelize"/>
+          <technology img="jquery.png" name="jQuery"/>
+          <technology img="vuejs.png" name="Vue.js"/>
+          <technology img="nuxtjs.png" name="Nuxt.js"/>
+          <technology img="laravel.png" name="Laravel"/>
+          <technology img="blade.png" name="Laravel Blade"/>
+          <technology img="twig.png" name="Twig"/>
+          <technology img="sass.png" name="Sass"/>
+          <technology img="stylus.png" name="Stylus"/>
+          <technology img="vuetify.png" name="Vuetify.js"/>
+          <technology img="materialize.png" name="Materialize"/>
+          <technology img="semantic-ui.png" name="Semantic UI"/>
+          <technology img="photoshop.png" name="Photoshop"/>
+          <technology img="illustrator.png" name="Illustrator"/>
+          <technology img="git.png" name="Git"/>
+          <technology img="cpp.png" name="C++"/>
+          <technology img="java.png" name="Java"/>
+          <technology img="mysql.png" name="MySQL"/>
+          <technology img="postgresql.png" name="PostgreSQL"/>
+          <technology img="drupal.png" name="Drupal"/>
+          <technology img="nuvemshop.png" name="Nuvem Shop"/>
         </v-layout>
       </section>
       <section>
-        <section-title title="Portfolio" anchor="portfolio"></section-title>
+        <section-title title="Portfolio" anchor="portfolio"/>
         <div class="text-xs-justify text-sm-center">
           Alguns dos projetos que já participei
         </div>
       </section>
       <section>
-        <section-title title="Contato" anchor="contact"></section-title>
+        <section-title title="Contato" anchor="contact"/>
         <div class="text-xs-justify text-sm-center">
           Se você estiver interessado no meu trabalho, quiser tirar uma dúvida, ou apenas bater um papo, estarei disponível nos seguintes meios:
           <v-layout row wrap justify-center class="mt-4">
@@ -70,10 +70,10 @@
             </v-flex>
           </v-layout>
           <v-layout row wrap justify-center class="mt-4 mb-3">
-            <social icon="mdi-facebook-box" color="#3b5998" href="https://www.facebook.com/DeehSlash" target="_blank"></social>
-            <social icon="mdi-instagram" color="#c32aa3" href="https://www.instagram.com/deehslash" target="_blank"></social>
-            <social icon="mdi-linkedin-box" color="#0077b5" href="https://www.linkedin.com/in/andreluiz97" target="_blank"></social>
-            <social icon="mdi-skype" color="#00aff0" href="skype:deeh1901?chat"></social>
+            <social icon="mdi-facebook-box" color="#3b5998" href="https://www.facebook.com/DeehSlash" target="_blank"/>
+            <social icon="mdi-instagram" color="#c32aa3" href="https://www.instagram.com/deehslash" target="_blank"/>
+            <social icon="mdi-linkedin-box" color="#0077b5" href="https://www.linkedin.com/in/andreluiz97" target="_blank"/>
+            <social icon="mdi-skype" color="#00aff0" href="skype:deeh1901?chat"/>
           </v-layout>
         </div>
       </section>
